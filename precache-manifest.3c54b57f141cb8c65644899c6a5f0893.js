d8054a5c58b025357d035db846d5d9d3937e5ed9
vovankiet <59630340+luuquanghoangdy@users.noreply.github.com>
2020-12-31 10:16:47 +0700

    Updates

---
+++ precache-manifest.3c54b57f141cb8c65644899c6a5f0893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1855b237faf3c4dd36f4264f5d689c",
+    "url": "/Cv-Kiet17c3/index.html"
+  },
+  {
+    "revision": "dba081be59db15362e10",
+    "url": "/Cv-Kiet17c3/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "af6f3a75e95212b0f93c",
+    "url": "/Cv-Kiet17c3/static/css/main.789a347f.chunk.css"
+  },
+  {
+    "revision": "dba081be59db15362e10",
+    "url": "/Cv-Kiet17c3/static/js/2.5434c593.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/Cv-Kiet17c3/static/js/2.5434c593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6f3a75e95212b0f93c",
+    "url": "/Cv-Kiet17c3/static/js/main.bba48d51.chunk.js"
+  },
+  {
+    "revision": "85534039bf160261f9fd",
+    "url": "/Cv-Kiet17c3/static/js/runtime-main.396a51e8.js"
+  }
+]);
